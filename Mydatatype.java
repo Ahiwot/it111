541403b701069f1e415ef05105e5a0ca7e893c35
Ahiwot <130112872+Ahiwot@users.noreply.github.com>
2023-04-21 15:28:48 -0700

    Add files via upload

---
+++ Mydatatype.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+
+public class Mydatatype {
+
+    public static void main(String[]args){
+
+        short number1= 32_000;
+         int number5= 2_000_000;
+         long number2= 3000_000_000l;
+         float number3= 50.57f;
+         double number4=50.45637377;
+
+         System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+
+
+        boolean dry=false;
+
+        if (dry) {
+
+            System.out.println("time to hang up this curtains");
+        }     else        {
+
+                System.out.println("we will have to wait a spell");
+            }
+        char best='A';
+        System.out.println(++best);
+        }
+
+    }
+
+
+
+
---
+++ concatenation.java
@@ -0,0 +1,14 @@
+public class concatenation {
+    public static void main(String[]args) {
+        String color = "red";
+        System.out.println("My favorite color is +color ");
+
+        int money = 2_000_000;
+        System.out.println("i hit the jackpot and i win $" + money + "dollar");
+
+        System.out.println(color+" "+money);
+        System.out.println("this will be my statement regarding my favorite color which is"+color+"and we will let you know that i won$"+ money+"dollars");
+    }
+
+    }
+
