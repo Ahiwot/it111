e87707257bef7915bb4ffc7c3c2d697f0e42b856
Ahiwot <130112872+Ahiwot@users.noreply.github.com>
2023-04-21 15:29:26 -0700

    Add files via upload

---
+++ MyFirstfile.java
@@ -0,0 +1,9 @@
+public class MyFirstfile {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("This is my first java file!!!");
+        System.out.println("This is what the program will run!!!");
+    }
+}
