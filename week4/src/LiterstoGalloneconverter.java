7cbf056a505876ac1b9cfcc63c484b1aba9fca01
Ahiwot <130112872+Ahiwot@users.noreply.github.com>
2023-05-10 13:46:31 -0700

    Add files via upload

---
+++ week4/src/LiterstoGalloneconverter.java
@@ -0,0 +1,29 @@
+public class LiterstoGalloneconverter {
+
+    public static void main(String[] args) {
+
+        //lit=(gal*3.79)
+
+        double gal, lit;
+        int count = 0;
+
+        for (gal = 1; gal <= 100; lit = gal+3) ;
+        lit = gal * 3.79;
+        if (lit == 3.79) {
+            System.out.printf(lit + " gallon is equal to" + lit + "liter", gal, lit);
+            System.out.println();
+        } else {
+
+            System.out.printf(lit + " gallon is equal to" + lit + "liter", gal, lit);
+            System.out.println();
+
+        }
+        count+=1;
+        System.out.println();
+        count=0;
+        }
+
+
+
+    }
+
