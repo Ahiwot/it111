f6d99f5cd99615a8aa931437bba07203caff2581
Ahiwot <130112872+Ahiwot@users.noreply.github.com>
2023-05-16 16:26:32 -0700

    Add files via upload

--- week5/src/DailySpecials.java
+++ week5/src/DailySpecials.java
@@ -10,54 +10,106 @@ public class DailySpecials {
         Scanner input = new Scanner(System.in);
 
         System.out.println("please enter a day of the week excluding weekends (monday-friday only!");
-
         specials = input.next();
-
 
         // in the world of coffee, I need a name of a coffee beverage and a price
 
         String coffee;
+        int quantity = 0;
         double price;
-      boolean saturday= specials.equals("saturday");
-        boolean sunday= specials.equals("sunday");
+        boolean saturday = specials.equals("saturday");
+        boolean sunday = specials.equals("sunday");
 
-      if(saturday|| sunday) {
-          System.out.println("please enter a week day not weekend");
-          specials=input.next();
-      }
+        if (saturday || sunday) {
+            System.out.println("please enter a week day not weekend");
+            specials = input.next();
+        }
 
         switch (specials) {
 
             //my cases will be the day of th week
-            case "Monday" -> {
-                coffee = "latte";
-                price = 4.95;
-                System.out.println(specials + " 's coffee of the day is a" + coffee + "and the price will be $" + price);
-                System.out.println(price);
+
+            coffee = "latte";
+            price = 4.95;
+            System.out.println(specials + " 's coffee of the day is a" + coffee + "and the price will be $" + price);
+            System.out.println(price);
+            System.out.println("how many latte would you like to order?");
+            specials = input.next();
+            if (quantity == 0) {
+                System.out.println("looks like you don't like latte! so sad!!!");
+
+            } else if (quantity > 0) {
+                System.out.println("looks like you will be ordering" + quantity + "coffee");
+
             }
             case "tuesday" -> {
-                coffee = "frapp";
+                coffee = "frappe";
                 price = 4.95;
                 System.out.println(specials + " 's coffee of the day is a" + coffee + "and the price will be $" + price);
                 System.out.println(price);
-            }
-            case "wednesday" -> {
-                coffee = "cappuccino";
-                price = 4.35;
-                System.out.println(specials + " 's coffee of the day is a" + coffee + "and the price will be $" + price);
-                System.out.println(price);
-            }
-            case "thursday" -> {
-                coffee = "regular joe";
-                price = 2.95;
-                System.out.println(specials + " 's coffee of the day is a" + coffee + "and the price will be $" + price);
-                System.out.println(price);
-            }
-            case "friday" -> {
-                coffee = " green tee latte";
-                price = 6.95;
-                System.out.println(specials + " 's coffee of the day is a" + coffee + "and the price will be $" + price);
-                System.out.println(price);
+                System.out.println("how many frappe would you like to order?");
+                specials = input.next();
+                if (quantity == 0) {
+                    System.out.println("looks like you don't like latte! so sad!!!");
+
+                } else if (quantity > 0) {
+                    System.out.println("looks like you will be ordering" + quantity + "coffee");
+
+
+                }
+
+                case "wednesday" -> {
+                    coffee = ("Cappuccino");
+                    price = 4.35;
+                    System.out.println(specials + " 's coffee of the day is a" + coffee + "and the price will be $" + price);
+                    System.out.println(price);
+                    System.out.println("how many cappuccino would you like to order?");
+                    specials = input.next();
+                    if (quantity == 0) {
+                        System.out.println("looks like you don't like latte! so sad!!!");
+
+                    } else if (quantity > 0) {
+                        System.out.println("looks like you will be ordering" + quantity + "coffee");
+
+
+                    }
+                    case "thursday" -> {
+                        coffee = ("regular joe");
+                        price = 2.95;
+                        System.out.println(specials + " 's coffee of the day is a" + coffee + "and the price will be $" + price);
+                        System.out.println(price);
+                        System.out.println("how many latte would you like to order?");
+                        specials = input.next();
+
+                        if (quantity == 0) {
+                            System.out.println("looks like you don't like latte! so sad!!!");
+
+                        } else if (quantity > 0) {
+                            System.out.println("looks like you will be ordering" + quantity + "coffee");
+                        }
+
+                        case "friday" -> {
+                            coffee = ("green tee latte");
+                            price = 4.35;
+                            System.out.println(specials + " 's coffee of the day is a" + coffee + "and the price will be $" + price);
+                            System.out.println(price);
+                            System.out.println("how many latte would you like to order?");
+                            specials = input.next();
+
+                            if (quantity == 0) {
+                                System.out.println("looks like you don't like latte! so sad!!!");
+
+                            } else if (quantity > 0) {
+                                System.out.println("looks like you will be ordering" + quantity + "coffee");
+
+                            }
+//
+//
+                        }
+
+                    }
+                }
+
             }
         }
 
